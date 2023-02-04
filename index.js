22883e2ab868487d89844cefae9d090eba7556c6
Ansh Vijay <anshvijay28@gmail.com>
2023-02-04 13:04:33 -0500

    added logic to make tiles black on hover and on mouseleave

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ let slider = document.querySelector('.slider');
 let sliderOutput = document.querySelector('.output');
 const container = document.querySelector('.grid-container');
 
+const colorButton = document.querySelector('#color-button');
+const resetButton = document.querySelector('#reset-button');
+const rainbowButton = document.querySelector('#rainbow-button');
+const gridButton = document.querySelector('#grid-button');
+
+
+
 container.style.gridTemplateColumns = "repeat(10, 1fr)"
 for (let i = 0; i < slider.value ** 2; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
 }
+const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.addEventListener('mouseleave', (e) => {
+        square.style.backgroundColor = "black";
+    }));
 
 const clearContainer = () => {
     while (container.firstChild) {
@@ -19,12 +30,15 @@ slider.addEventListener('input', function() {
     sliderOutput.textContent = slider.value;
     let dimensionStyle = "repeat(" + slider.value + ", 1fr)";
     container.style.gridTemplateColumns = dimensionStyle;
-    let num = 0;
     for (let i = 0; i < slider.value ** 2; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
     }
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.addEventListener('mouseleave', (e) => {
+        square.style.backgroundColor = "black";
+    }));
 });
 
 
