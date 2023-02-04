749b7bfa438f155c61643e4ff63dc9e80ee2dae5
Ansh Vijay <anshvijay28@gmail.com>
2023-02-04 13:48:16 -0500

    added logic for functioning reset button

--- index.js
+++ index.js
@@ -7,24 +7,43 @@ const resetButton = document.querySelector('#reset-button');
 const rainbowButton = document.querySelector('#rainbow-button');
 const gridButton = document.querySelector('#grid-button');
 
-
-
-container.style.gridTemplateColumns = "repeat(10, 1fr)"
-for (let i = 0; i < slider.value ** 2; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-}
-const squares = document.querySelectorAll(".square");
-    squares.forEach(square => square.addEventListener('mouseleave', (e) => {
+function reset() {
+    clearContainer();
+    container.style.gridTemplateColumns = "repeat(10, 1fr)"
+    for (let i = 0; i < 100; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+    resetSlider();
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.addEventListener('mouseleave', e => {
         square.style.backgroundColor = "black";
     }));
+}
 
-const clearContainer = () => {
+function clearContainer() {
     while (container.firstChild) {
         container.removeChild(container.lastChild);
     }
 }
+function resetSlider() {
+    sliderOutput.textContent = 10;
+    while (slider.value != 10) {
+        if (slider.value > 10) {
+            slider.stepDown();
+        } else {
+            slider.stepUp();
+        }
+    }
+}
+
+reset();
+
+resetButton.addEventListener("click", e => {
+    reset();
+});
+
 slider.addEventListener('input', function() {
     clearContainer();
     sliderOutput.textContent = slider.value;
